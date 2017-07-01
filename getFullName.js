2dacd7510296f54ddbcaff8f8829fc2000414439
ledet awano <ledet2016@gmail.com>
2017-07-01 16:21:30 -0500

    Create getFullName.js

---
+++ getFullName.js
@@ -0,0 +1,9 @@
+//Given a first and a last name, "getFullName" returns a single string with 
+// the given first and last names separated by a single space.
+
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName // create a return statement that will output the first and last name
+                                    // with a space between the two
+}
+
+getFullName('mary', 'kate'); // 'mary kate'
