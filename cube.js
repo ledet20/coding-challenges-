df9106daf65d0c895b9a7b623e425efb38b36370
ledet awano <ledet2016@gmail.com>
2017-07-01 17:26:06 -0500

    Create cube.js

---
+++ cube.js
@@ -0,0 +1,8 @@
+// Given a number, "cube" returns the cube of that number.
+
+function cube(num) {
+  return Math.pow(num, 3); // using a javascript built in function Math.pow take two parameters 
+                           // the base number and the exponent in which the number will be raised to 
+}
+
+cube(2); // 8 
