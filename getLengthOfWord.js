64b4397defc3b59f55b32e68d8ca438fe29193ac
ledet awano <ledet2016@gmail.com>
2017-07-01 16:24:06 -0500

    Create getLengthOfWord.js

---
+++ getLengthOfWord.js
@@ -0,0 +1,7 @@
+// Given a word, "getLengthOfWord" returns the length of the given word.
+
+function getLengthOfWord(word) {
+  return word.length;  // return statement that gets the length of the word passed through the parameter 
+}
+
+getLengthOfWord('black'); // 5
