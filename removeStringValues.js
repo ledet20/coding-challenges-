7846b2d41e56d3ad953be8c45d5a44096291d06f
ledet awano <ledet2016@gmail.com>
2017-07-02 12:33:16 -0500

    Update removeStringValues.js

--- removeStringValues.js
+++ removeStringValues.js
@@ -11,6 +11,7 @@ function removeStringValues(obj) {
     for(var prop in obj){ // for in loop that will iterate through the key value pairs 
     if (typeof obj[prop] === 'string') { // if statement that will determine if the value of a key fhl
                                          //  is a string or not 
+                                         // typeof operater will determine if the value is a string 
       delete obj[prop];                  // if the statement is true it will delete the key value pair 
     }
   }
