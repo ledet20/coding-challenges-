2af966e67b688df256b7898f09c2d14e0500fe90
ledet awano <ledet2016@gmail.com>
2017-07-01 21:01:42 -0500

    Create computePower.js

---
+++ computePower.js
@@ -0,0 +1,9 @@
+// Given a number and an exponent, "computePower" returns the given number, 
+// raised to the given exponent. 
+
+function computePower(num, exponent) {
+  return Math.pow(num, exponent);  // javascript built in function MAth.pow() takes in two parameters 
+                                  // the base number and the exponenet you want to raise it to 
+}
+
+computePower(2,2); // 4
