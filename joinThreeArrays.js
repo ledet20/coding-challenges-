26ee49ca9ec64b2990c20fc05541dc694124335a
ledet awano <ledet2016@gmail.com>
2017-07-03 17:25:31 -0500

    Update joinThreeArrays.js

--- joinThreeArrays.js
+++ joinThreeArrays.js
@@ -2,7 +2,7 @@
 //of "arr1" in order followed by the elements in "arr2" in order followed by the elements of "arr3" in order.
 
 function joinThreeArrays(arr1, arr2, arr3) {
-  var newArray = arr1.concat(arr2,arr3); // create a variable to storeyou new array
+  var newArray = arr1.concat(arr2,arr3); // create a variable to store your new array
             // use teh javascript built in method .concat to join all 3 arrays together
   return newArray; // return the new array
 }
